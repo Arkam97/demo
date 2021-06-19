93f7938975dccf09b47cdcc4da4d2d1180d332a0
Arkam97 <husainmohamedarkam1997@gmail.com>
2021-06-20 00:11:32 +0530

    change after deploy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
        <h1>Hi im Arkam</h1>
        <h2>this is now updated</h2>
+       <h2>Hi ....................</h2>
       </header>
     </div>
   );
